f1dfec1ca4bf0827f674c9cbdb0be81b33b2d384
Ayomide <ayomidotzee@gmail.com>
2023-03-13 09:57:57 -0700

    Page switch tracked

--- src/Components/Aside.tsx
+++ src/Components/Aside.tsx
@@ -83,27 +83,29 @@ export default function Aside() {
     }
 
     return (
-        <div className='fixed bg-[#2B2C37] w-[18.75rem] hidden md:flex gap-2 px-4 py-6 md:px-10 border-b-[1px] border-r-[1px] border-[#8686861a]' style={style}>
+        <div className='fixed bg-[#2B2C37] w-[18.75rem] hidden md:flex gap-2 py-6 border-b-[1px] border-r-[1px] border-[#8686861a]' style={style}>
             <div>
-                <h3 className='font-semibold text-[#828fa3] text-[0.75rem] mb-4 tracking-[2.4px]'>{`ALL BOARDS (${Boards.length})`}</h3>
-                <div>
+                <h3 className='font-semibold text-[#828fa3] text-[0.75rem] mb-4 tracking-[2.4px] px-4 md:px-10'>{`ALL BOARDS (${Boards.length})`}</h3>
+                <div className='max-h-[400px] overflow-y-scroll no-scrollbar'>
                     {Boards.map(board=>{
                         //Aside Boards
                         return (
-                            <div key={board.id} onClick={()=>changeBoard(board.id)} className='flex items-center gap-2 text-left pb-2 min-h-[2.8rem] h-fit mb-[0.2rem] relative bg-transparent cursor-pointer transition-colors delay-200 ease-in'>
-                                <i className='text-[#828fa3] text-[1.5rem]'><TbLayoutBoardSplit /></i>
-                                <p className='text-[1rem] text-[#828fa3] font-semibold'>{board.name}</p>          
+                            <div key={board.id} onClick={()=>changeBoard(board.id)} className={`flex ${board.isActive ? 'text-white bg-[#635FC7]': 'text-[#828fa3] bg-transparent hover:bg-[#635FC7] hover:opacity-70'} px-4 md:px-10 rounded-r-full items-center gap-2 text-left min-h-[2.8rem] h-fit mb-[0.2rem] relative cursor-pointer transition-all delay-200 ease-in`}>
+                                <i className='text-[1.5rem]'><TbLayoutBoardSplit /></i>
+                                <p className='text-[1rem] font-semibold'>{board.name}</p>          
                             </div>
                         )
                     })}
-                    <div className='flex items-center gap-2 text-left pb-2 min-h-[2.8rem] h-fit mb-[0.2rem] cursor-pointer relative bg-transparent transition-colors delay-200 ease-in'>
+                </div>
+                <div>
+                    <div className='flex px-4 md:px-10 hover:opacity-70 items-center gap-2 text-left pb-2 min-h-[2.8rem] h-fit mb-[0.2rem] cursor-pointer relative bg-transparent transition-colors delay-200 ease-in'>
                         <i className='text-[#635fc7] text-[1.5rem]'><TbLayoutBoardSplit /></i>
                        <div onClick={generateBoard} className='flex items-center gap-1 text-[1rem] text-[#635fc7] font-semibold'>
                             <i className='text-[0.6rem]'><GoPlus /></i>
                             <p>Create New board</p>
                         </div>       
                     </div>
-                    <div className='flex items-center gap-2 text-left pb-2 min-h-[2.8rem] h-fit mb-[0.2rem] cursor-pointer relative bg-transparent transition-colors delay-200 ease-in'>
+                    <div className='flex px-4 md:px-10 hover:opacity-70 items-center gap-2 text-left pb-2 min-h-[2.8rem] h-fit mb-[0.2rem] cursor-pointer relative bg-transparent transition-colors delay-200 ease-in'>
                         <i className='text-[#ba3232] text-[1.5rem]'><TbLayoutBoardSplit /></i>
                        <div onClick={deleteAccount} className='flex items-center gap-1 text-[1rem] text-[#ba3232] font-semibold'>
                             <i className='text-[0.6rem]'><GoPlus /></i>
---
+++ src/Components/AsideFooter.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AsideFooter() {
+    return (
+        <div className='px-4 md:px-10'>
+        
+        </div>
+    )
+}
