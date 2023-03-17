a2ec819c6da5b2d3b02e3e661155cb1d9adb0e80
Ayomide <ayomidotzee@gmail.com>
2023-03-16 17:16:32 -0700

    Can create new Columns

--- src/Components/Modals/AddColumnModal.tsx
+++ src/Components/Modals/AddColumnModal.tsx
@@ -69,6 +69,32 @@ export default function AddColumnModal() {
       })
     }
   }
+
+  function saveChangesToBoard(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    if(currentBoard && currentBoardCopy){
+      const newColumns = currentBoardCopy?.columns.filter(item=>item.name !=='') ?? []
+      const newBoards: BoardType[] = Boards.map(items=>{
+        if(items.id === currentBoard.id){
+          return {
+            ...currentBoardCopy,
+            columns: newColumns
+          }
+        }else {
+          return items
+        }
+      })
+      dispatch({
+        type: 'setBoards',
+        payload: {
+          BoardsPayload: newBoards
+        }
+      })
+      dispatch({
+        type: 'setNoModals'
+      })
+    }
+  }
   
   return (
     <>
@@ -77,7 +103,7 @@ export default function AddColumnModal() {
       <div ref={modalRef} className='rounded-[10px] bg-[#2B2C37] w-full max-w-[30rem] min-w-[350px] p-8 fixed top-0 md:top-[10%] z-[99999] h-full md:min-h-[250px] md:max-h-[550px] md:flex md:flex-col'>  
           <button onClick={cancel} className='absolute md:hidden top-[0.5rem] right-[0.3rem] rounded-[4px] p-[0.3rem] bg-[#0808081a] text-white'><FaTimes /></button>
           <h3 className='mb-4 text-[1.125rem] font-semibold text-white'>Add New Column</h3>
-          <form>
+          <form onSubmit={saveChangesToBoard}>
             <div className='flex flex-col'>
               <h3 className='text-[0.75rem] font-semibold text-white mb-2'>Name</h3>
               <p className=' w-full bg-transparent border-[2px] rounded-md px-4 py-2 text-[0.8125rem] font-semibold text-[#b5aaaa9b] transition-colors delay-200 ease-linear outline-none focus:border-[#635FC7] border-[#828ca366] '>
@@ -93,6 +119,25 @@ export default function AddColumnModal() {
                       type='text'
                       defaultValue={item.name}
                       className=' w-[90%] bg-transparent border-[2px] rounded-md px-4 py-2 text-[0.8125rem] font-semibold text-white transition-colors delay-200 ease-linear outline-none focus:border-[#635FC7] border-[#828ca366] '
+                      value={item.name}
+                      onChange={(e)=>{
+                        dispatch({
+                          type: 'setCurrentBoardCopy',
+                          payload: {
+                            currentBoardCopyPayload: {
+                              ...currentBoardCopy,
+                              columns: currentBoardCopy.columns.map(c=>{
+                                if(item.id === c.id){
+                                  return {
+                                    ...c,
+                                    name: e.target.value
+                                  }
+                                }else return c
+                              })
+                            }
+                          }
+                        })
+                      }}
                     />
                     <button type='button' onClick={(e)=>{e.stopPropagation(); deleteItem(item.id)}} className='text-[#808080] opacity-20 text-[1.5rem]'><FaTimes /></button>
                   </label>
@@ -101,7 +146,7 @@ export default function AddColumnModal() {
             </div>
             <div className='mt-4 flex flex-col gap-4 w-full'>
               <button type='button' onClick={()=>addColumn()} className='font-semibold rounded-full py-2 text-[0.8125rem] flex items-center justify-center bg-white text-[#635fc7]'>+ Add New Column</button>
-              <button className='font-semibold rounded-full py-2 text-[0.8125rem] flex items-center justify-center bg-[#635fc7] text-white'>Save Chages</button>
+              <button type='submit' className='font-semibold rounded-full py-2 text-[0.8125rem] flex items-center justify-center bg-[#635fc7] text-white'>Save Chages</button>
             </div>
           </form>
       </div>
