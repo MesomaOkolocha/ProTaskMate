a68f6be61ca87e9b912955d3de88eb03b1c0a3a9
Ayomide <ayomidotzee@gmail.com>
2023-03-27 08:57:46 -0700

    Added license and removed API KEYS

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,13 +9,13 @@ import { getDatabase } from 'firebase/database'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGrIhuR4tljso4XOiwTihMi3-jPA3_axI",
-  authDomain: "protaskmate.firebaseapp.com",
-  projectId: "protaskmate",
-  storageBucket: "protaskmate.appspot.com",
-  messagingSenderId: "742993071498",
-  appId: "1:742993071498:web:bc27f0622c5c13afb14c73",
-  measurementId: "G-9221MEC3E9"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
